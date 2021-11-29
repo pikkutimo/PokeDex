c69111c7bcb1c8b216e5be6daba810fbed9f41f9
Timo Vilen <timo.a.vilen@tuni.fi>
2021-11-29 10:32:36 +0200

    Slight tweak to the parameter of the image.

--- screens/CardScreen.js
+++ screens/CardScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {  Text, View, Button, Image, StyleSheet } from 'react-native';
+import {  View, Image, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
 
